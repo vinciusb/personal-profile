bd4fbce0cc32167467f7d2682a4db56d30fffd90
vinciusb <65973642+vinciusb@users.noreply.github.com>
2022-12-26 17:34:11 -0300

    portfolio base

--- src/Components/Pages/ProgressPage/ProgressPage.tsx
+++ src/Components/Pages/ProgressPage/ProgressPage.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { ProgressPageProps } from '../../../interfaces/interface';
 
 import './ProgressPage.scss';
 
+const N_PROJECTS = 3;
+
 const ProgressPage: React.FC<ProgressPageProps> = ({ className }: ProgressPageProps) => {
-    return <div className={'progress-page ' + className}></div>;
+    const [count, setCount] = useReducer((state: number, mode: boolean): number => {
+        if (mode) {
+            return state === N_PROJECTS - 1 ? 0 : state + 1;
+        }
+        return state === 0 ? N_PROJECTS - 1 : state - 1;
+    }, 0);
+
+    const renderPoints = (): JSX.Element => {
+        return (
+            <div className="progress-points">
+                {Array.from(Array(N_PROJECTS), (_, id) => (
+                    <div key={id} className={'point ' + (id === count ? 'highlighted' : '')}></div>
+                ))}
+            </div>
+        );
+    };
+
+    return (
+        <div className={'progress-page ' + className}>
+            <div className="portfolio secao">
+                <div id="past" className="transition-button" onClick={() => setCount(false)}>
+                    {'<'}
+                </div>
+                <div className="main-frame"></div>
+                <div id="next" className="transition-button" onClick={() => setCount(true)}>
+                    {'>'}
+                </div>
+                {renderPoints()}
+            </div>
+
+            <div className="stacks secao"></div>
+        </div>
+    );
 };
 
 export default ProgressPage;
